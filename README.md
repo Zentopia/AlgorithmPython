f2bd6364c641a83f91e3c666593c9b7f9ac035d1
Utopia <izentopia@gmail.com>
2017-10-31 17:10:40 +0800

    方法封装

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AlgorithmPython
 Algorithm in Python
+
+## 已收录的算法：
+- [链表反转][1]
+
+[1]:	https://github.com/Zentopia/AlgorithmPython/blob/master/reverse_linked_list.py
--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -4,37 +4,45 @@ class Node:
         self.data = value
         self.next = None
 
-#创建链表
-head_node = Node(0)
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
+# 链表反转
+def reverse(head_node):
 
-head_node.next = node1
-node1.next = node2
-node2.next = node3
-node3.next = node4
+    next_node = head_node.next
+    prev_node = head_node
 
-#开始链表反转
-temp_node = head_node.next
-current_node = head_node
+    #第一个节点反转后称为最后一个节点
+    prev_node.next = None
 
-#第一个节点反转后称为最后一个节点
-current_node.next = None
+    while True:
+        head_node = next_node
+        next_node = head_node.next
 
-while True:
-    head_node = temp_node
-    temp_node = head_node.next
+        #反转
+        head_node.next = prev_node
 
-    #反转
-    head_node.next = current_node
+        if next_node == None:
+            break
+        prev_node = head_node
 
-    if temp_node == None:
-        break
-    current_node = head_node
+    return head_node
 
-#打印反转后的链表
-while head_node != None:
-    print(head_node.data)
-    head_node = head_node.next
+if __name__ == '__main__':
+
+    # 创建链表
+    head_node = Node(0)
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+
+    head_node.next = node1
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+
+    head_node = reverse(head_node)
+
+    # 打印反转后的链表
+    while head_node != None:
+        print(head_node.data)
+        head_node = head_node.next
