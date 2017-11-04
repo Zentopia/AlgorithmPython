2eab7868cbf4b287b11ccdc871d57ce4d401df32
Utopia <gejunwei110@gmail.com>
2017-11-04 23:32:27 +0800

    宝宝写的

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -63,3 +63,48 @@ if __name__ == '__main__':
     while head_node != None:
         print(head_node.data)
         head_node = head_node.next
+        
+    """
+    class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+def recursive_reverse(reversed_node, next_node):
+    if next_node == None:
+        return reversed_node
+    new_node = next_node.next
+    next_node.next = reversed_node
+    return recursive_reverse(next_node, new_node)
+
+def loop(head_node):
+    second_node = head_node.next
+    third_node = second_node.next
+    head_node.next = None
+    second_node.next = head_node
+    while third_node!=None:
+        head_node = second_node
+        second_node = third_node
+        third_node = third_node.next
+        second_node.next = head_node
+    return second_node
+
+def print_node(head_node):
+    while(head_node!=None):
+        print(head_node.data)
+        head_node = head_node.next
+
+if __name__ == '__main__':
+    head_node = Node(0)
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    head_node.next = node1
+    node1.next = node2
+    node2.next = node3
+    # head_node.next = None
+    # head_node = recursive_reverse(head_node,node1)
+    head_node = loop(head_node)
+    print_node(head_node)
+ """
