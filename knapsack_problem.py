acbf74a1db695da5f122ace9325f8fe28dd4dc88
Utopia <gejunwei110@gmail.com>
2017-11-14 23:03:42 +0800

    增加宝宝写的

--- knapsack_problem.py
+++ knapsack_problem.py
@@ -26,3 +26,20 @@ while n > 0:
         c = c - weight[n]
         n = n - 1
 
+"""宝宝写的
+num = 4
+capacity = 7
+value = [0,1,4,5,7]
+weight = [0,1,3,4,5]
+result = [[0 for i in range(capacity+1)] for y in range(num+1)]
+for n in range(num+1):
+    for c in range(1, capacity+1):
+        if weight[n]<= c:
+            print("n:",n,"c:",c)
+            result[n][c] = max(value[n] + result[n - 1][c - weight[n]], result[n - 1][c])
+            print("result:",result[n][c],  result[n - 1][c - weight[n]], "index", c - weight[n])
+            pass
+        else:
+            result[n][c] = result[n - 1][c]
+print(result)
+"""
